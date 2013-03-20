f73a0d58d4f564ccef5a80a274fcccef90b1e669
shimizukawa <shimizukawa@gmail.com>
2013-03-20 20:11:59 +0900

    fix: node hash-key need 'string' instead :symbol for json node definition

--- attributes/default.rb
+++ attributes/default.rb
@@ -26,27 +26,27 @@ while pw.length < 20
 end
 
 # for recipes/default.rb
-default[:rvm_redmine][:rvm_name] = "@redmine"
-default[:rvm_redmine][:user] = "www-data"
-default[:rvm_redmine][:group] = "root"
-default[:rvm_redmine][:user_home] = "/var/www"
-default[:rvm_redmine][:install_prefix] = "/var/www/www-data"
-default[:rvm_redmine][:dl_id]    = "76130"
-default[:rvm_redmine][:name] = "redmine-1.4.2"
-default[:rvm_redmine][:file] = nil  # auto generate from :name
-default[:rvm_redmine][:archive_src] = nil  # auto generate from :dl_id and :file
+default["rvm_redmine"]["rvm_name"] = "@redmine"
+default["rvm_redmine"]["user"] = "www-data"
+default["rvm_redmine"]["group"] = "root"
+default["rvm_redmine"]["user_home"] = "/var/www"
+default["rvm_redmine"]["install_prefix"] = "/var/www/www-data"
+default["rvm_redmine"]["dl_id"]    = "76130"
+default["rvm_redmine"]["name"] = "redmine-1.4.2"
+default["rvm_redmine"]["file"] = nil  # auto generate from :name
+default["rvm_redmine"]["archive_src"] = nil  # auto generate from :dl_id and :file
 
-default[:rvm_redmine][:db][:type]     = "mysql"
-default[:rvm_redmine][:db][:user]     = "root"
-default[:rvm_redmine][:db][:password] = pw
-default[:rvm_redmine][:db][:hostname] = "localhost"
+default["rvm_redmine"]["db"]["type"]     = "mysql"
+default["rvm_redmine"]["db"]["user"]     = "root"
+default["rvm_redmine"]["db"]["password"] = pw
+default["rvm_redmine"]["db"]["hostname"] = "localhost"
 
-default[:rvm_redmine][:unicorn_port] = '10080'
+default["rvm_redmine"]["unicorn_port"] = '10080'
 
-default[:rvm_redmine][:plugins] = []
+default["rvm_redmine"]["plugins"] = []
 
 # for recipes/apache.rb
-default[:rvm_redmine][:hostname]         = 'localhost'
-default[:rvm_redmine][:hostname_aliases] = []
+default["rvm_redmine"]["hostname"]         = 'localhost'
+default["rvm_redmine"]["hostname_aliases"] = []
 
 
