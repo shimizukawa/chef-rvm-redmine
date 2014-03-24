f902fee3d1366dc9bf43e5de06e22abfb832c68b
shimizukawa <takayuki.shimizukawa@beproud.jp>
2014-03-25 08:59:17 +0900

    bump version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "shimizukawa@gmail.com"
 license          "Apache 2.0"
 description      "Installs/Configures redmine"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          "0.1.1"
 
 recipe "rvm-redmine", "install redmine into gem environment."
 recipe "rvm-redmine::apache", "install apache and setup proxy to redmine."
